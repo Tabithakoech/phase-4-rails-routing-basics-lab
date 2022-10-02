84319814e649c1f1e4a91c62c0042854924d6964
TabithaKoech <tabbyk1718@gmail.com>
2022-10-02 21:34:51 +0300

    Done

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,19 @@
+
+
+class StudentsController < ApplicationController
+
+    def index
+        all_students = Student.all
+        render json: all_students
+    end
+
+    def grades
+        graded_students = Student.order(grade: :desc)
+        render json: graded_students 
+    end
+
+    def highest_grade
+        highest_grade_student = Student.order(grade: :desc).first
+        render json: highest_grade_student
+    end
+end
